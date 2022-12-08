d58045ea553db5bcb10a922b6985679692ea1695
Belll5145 <Belll@wlhs.wlwv.k12.or.us>
2022-12-08 09:43:00 -0800

    Examples done

--- src/MoreLoopFun.java
+++ src/MoreLoopFun.java
@@ -7,18 +7,27 @@ public class MoreLoopFun {
          */
 
         //a for loop that counts by ones from 1 to 5
-
+        for(int start = 1; start <= 6; start++){
+            System.out.println(start);
+        }
 
         //a for loop that prints the powers of two up to 100
         //can you have an infinite for loop?
-
-
+        for(int power = 1; power < 100; power *= 2){
+            System.out.print(power + " ");
+        }
+        System.out.println();
         //write a for loop that counts by 4s from 5 up to 50
-
-
+        for(int fours = 5; fours <= 50; fours += 4){
+            System.out.print(fours +" ");
+        }
+        System.out.println();
         //write a for loop that sums the numbers from 1 to 10
         int total = 0;
-
+        for(int i = 1; i <= 10; i++){
+            total += i;
+        }
+        System.out.println("The sum of the integers 1 through 10 is: "+ total);
         ////////////////////////////////////////////////////
         //for loops compared to while loops////////////////
         //////////////////////////////////////////////////
